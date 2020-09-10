0cdb91e56508074b532246e9172e63d334f9beef
Teale Foster <LivingLegend@Teales-MBP-2.lan1>
2020-09-10 18:51:48 -0500

    added mentorship_eligibility.csv

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -28,3 +28,21 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT DESC;
+
+-- Create a table for mentorship_eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_employee as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+AND de.to_date = '9999-01-01'
+ORDER BY e.emp_no
