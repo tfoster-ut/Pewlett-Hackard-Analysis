3eb32f772bf9869192f3f26f645522d1a30cc825
Teale Foster <LivingLegend@Teales-MacBook-Pro-2.local>
2020-09-10 17:02:46 -0500

    added retiring_titles csv

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -12,7 +12,6 @@ ON e.emp_no = t.emp_no
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
-
 -- Create unique titles table
 SELECT DISTINCT ON (rt.emp_no) emp_no,
 	rt.first_name,
@@ -21,3 +20,11 @@ SELECT DISTINCT ON (rt.emp_no) emp_no,
 INTO unique_titles
 FROM retirement_titles as rt
 ORDER BY rt.emp_no ASC, rt.to_date DESC;
+
+-- Create a table of retirements by department (count)
+SELECT COUNT (first_name),
+	ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT DESC;
