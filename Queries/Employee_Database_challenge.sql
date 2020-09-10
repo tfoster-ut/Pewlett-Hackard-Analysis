a3241bd33a7d574c20acc44d7099833134872a2b
Teale Foster <LivingLegend@Teales-MacBook-Pro-2.local>
2020-09-10 16:48:19 -0500

    added unique titles table/csv

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,23 @@
+-- Create table of retirement titles
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+
+-- Create unique titles table
+SELECT DISTINCT ON (rt.emp_no) emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no ASC, rt.to_date DESC;
